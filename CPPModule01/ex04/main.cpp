51333d4c0ceee4733916616a633e789ba3968e88
Edison Lim <babyedison@gmail.com>
2022-06-15 10:44:49 +0800

    ex04 done

--- CPPModule01/ex04/main.cpp
+++ CPPModule01/ex04/main.cpp
@@ -2,20 +2,15 @@
 
 static string sed(string text, string s1, string s2)
 {
-	int		i = 0;
-	string newtext;
 	size_t pos = 0;
 	size_t len = s1.length();
 
 	while((pos = text.find(s1)) != string::npos)
 	{
-		newtext += text.substr(i, pos);
-		newtext += s2;
 		text.erase(pos, len);
-		i = pos;
+		text.insert(pos, s2);
 	}
-	newtext += text.substr(i, (text.length() - i));
-	return (newtext);
+	return (text);
 }
 
 static void replacefile(string file, string text)
@@ -36,6 +31,8 @@ int	main(int argc, char **argv)
 	if (argc != 4)
 		return (-1);
 	file.open(argv[1]);
+	if (!file)
+		return (-1);
 	while (getline(file, line))
 	{
 		text += line;
@@ -45,6 +42,5 @@ int	main(int argc, char **argv)
 	file.close();
 	text = sed(text, (string)argv[2], (string)argv[3]);
 	replacefile(argv[1], text);
-	std::cout << text;
 	return (0);
 }
