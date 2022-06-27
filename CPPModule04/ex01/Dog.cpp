9252b097c744fd6630715d33112894257f452836
PandieYay <babyedison@gmail.com>
2022-06-27 15:40:00 +0800

    ex01 done

--- CPPModule04/ex01/Dog.cpp
+++ CPPModule04/ex01/Dog.cpp
@@ -7,17 +7,21 @@ void Dog::makeSound(void) const{
 Dog::Dog(){
 	cout << "Dog constructor called" << std::endl;
     _type = "Dog";
+    brain = new Brain();
 }
 
 Dog::Dog(const Dog &a){
     _type = a._type;
+    brain = new Brain();
 }
 
 Dog &Dog::operator=(const Dog &a){
     _type = a._type;
+    brain = new Brain();
 	return *this;
 }
 
 Dog::~Dog(){
 	cout << "Dog destructor called" << std::endl;
+    delete brain;
 }
--- CPPModule04/ex01/Dog.hpp
+++ CPPModule04/ex01/Dog.hpp
@@ -2,11 +2,14 @@
 #define DOG_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 #include <string>
 
 using std::string;
 
 class Dog: public Animal {
+    private:
+        const Brain* brain;
     public:
         Dog();
 		Dog(const Dog &a);
