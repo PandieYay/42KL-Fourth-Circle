0ba2bc21d5460622852e9ab301170148b843c92a
PandieYay <babyedison@gmail.com>
2022-09-10 12:37:16 +0800

    Added extra test for ex02

--- CPPModule04/ex02/main.cpp
+++ CPPModule04/ex02/main.cpp
@@ -20,12 +20,6 @@ int main()
 	for (int i= 0; i < size; i++)
 		delete array[i];
 
-    // const Animal* j = new Dog();
-    // const Animal* i = new Cat();
-    // delete j;//should not create a leak
-    // delete i;
-
-    // Dog basic;
-    // Dog tmp = basic;
+	// Animal Test;
     return 0;
 }
